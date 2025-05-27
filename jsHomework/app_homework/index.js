e7966ffec80ee1cb679a2e55b8c5dfd14bdd9fb8
heejin-gaebal <pheejin603@gmail.com>
2025-05-27 17:52:35 +0900

    Add files via upload

---
+++ jsHomework/app_homework/index.js
@@ -0,0 +1,18 @@
+const recipeArry = [];  //레시피 배열
+
+function addList(){
+    //재료 입력받기
+    //수량 입력받기
+    const ingrdnt = document.querySelector("#ingrdnt").value;
+    const qntty = document.querySelector("#qntty").value;
+
+    const obj = { //묶어서 객체로 표현
+        ingrdnt : ingrdnt,
+        qntty : qntty
+    };
+    recipeArry.push()
+}
+
+function completeList(){
+
+}
